7dea7ab35613485a91b86fc78034ffec8a25e7da
arisgi <m1so.tama16@gmail.com>
2016-09-15 01:55:49 +0900

    Add template to js

---
+++ js/spa.shell.js
@@ -0,0 +1,59 @@
+/*
+ * spa.shell.js
+ * spa shell module
+*/
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+/*global $, spa */
+spa.shell = (function () {
+  //---------- module scope var ---------------------------
+  var
+    configMap = {
+      main_html : String()
+        + '<div class="spa-shell-head">'
+          + '<div class="spa-shell-head-logo"></div>'
+          + '<div class="spa-shell-head-acct"></div>'
+          + '<div class="spa-shell-head-search"></div>'
+        + '</div>'
+        + '<div class="spa-shell-main">'
+          + '<div class="spa-shell-main-nav"></div>'
+          + '<div class="spa-shell-main-content"></div>'
+        + '</div>'
+        + '<div class="spa-shell-foot"></div>'
+        + '<div class="spa-shell-chat"></div>'
+        + '<div class="spa-shell-modal"></div>'
+    },
+    stataMap  = { $container : null },
+    jqueryMap = {},
+
+    setJqueryMap, initModule;
+    //---------- end module scope var ---------------------
+
+    //---------- utility method ---------------------------
+    //---------- end utility method -----------------------
+
+    //---------- DOM method -------------------------------
+    // DOM method /setJqueryMap/
+    setJqueryMap = function () {
+      var $container = stateMap.$container;
+      jqueryMap = { $container : $container };
+    };
+    //---------- end DOM method ---------------------------
+
+    //---------- event handler ----------------------------
+    //---------- end event handler ------------------------
+
+    //---------- public method ----------------------------
+    initModule = function ( $container ) {
+      stateMap.$container = $container;
+      $container.html( configMap.main_html );
+      setJqueryMap();
+    };
+
+    return { initModule : initModule };
+    //---------- end public method ------------------------
+}());
