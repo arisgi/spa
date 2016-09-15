b38dad1f2ac0ebf61f675864042767d946c03416
arisgi <m1so.tama16@gmail.com>
2016-09-15 18:18:27 +0900

    Add anchor

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -114,7 +114,12 @@ spa.shell = (function () {
 
     //---------- event handler ----------------------------
     onClickChat = function ( event ) {
-      toggleChat( stateMap.is_chat_retracted );
+      if ( toggleChat( stateMap.is_chat_retracted ) ) {
+        $.uriAnchor.setAnchor({
+          chat : ( stateMap.is_chat_retracted ? 'open' : 'closed' )
+        });
+      }
+
       return false;
     };
     //---------- end event handler ------------------------
