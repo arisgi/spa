350fd04c7572ecd70fbf59413831365ee88d175d
arisgi <m1so.tama16@gmail.com>
2016-09-21 15:56:49 +0900

    Add avatar

--- js/spa.model.js
+++ js/spa.model.js
@@ -163,7 +163,8 @@ spa.model = (function (){
     var
       _publish_listchange, _publish_updatechat,
       _update_list, _leave_chat,
-      get_chatee, join_chat, send_msg, set_chatee,
+      get_chatee, join_chat, send_msg,
+      set_chatee, update_avatar,
       chatee = null;
 
     _update_list = function ( arg_list ) {
@@ -281,12 +282,20 @@ spa.model = (function (){
       return true;
     };
 
+    update_avatar = function ( avatar_update_map ) {
+      var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+      if ( sio ) {
+        sio.emit( 'updateavatar', avatar_update_map );
+      }
+    };
+
     return {
-      _leave     : _leave_chat,
-      get_chatee : get_chatee,
-      join       : join_chat,
-      send_msg   : send_msg,
-      set_chatee : set_chatee
+      _leave        : _leave_chat,
+      get_chatee    : get_chatee,
+      join          : join_chat,
+      send_msg      : send_msg,
+      set_chatee    : set_chatee,
+      update_avatar : update_avatar
     };
   }());
 
